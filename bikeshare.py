c921baf0359b1c89ee0db6bc377fb2a01607895a
Mahmoud Shihab <mahmoud.a.shihab@gmail.com>
2019-07-16 19:44:20 +0200

    Improve restart loop

--- bikeshare.py
+++ bikeshare.py
@@ -189,6 +189,7 @@ def user_stats(df):
 
 
 def main():
+    yes_values = ['yes', 'y']
     while True:
         city, month, day = get_filters()
         df = load_data(city, month, day)
@@ -199,7 +200,7 @@ def main():
         user_stats(df)
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
-        if restart.lower() != 'yes':
+        if restart.lower() not in yes_values:
             break
 
 
